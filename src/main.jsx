4575e21b9d3f5ec91d369e114681a15ab79dea48
Amitchaudhary0 <amitchaudhary2022.23@gmail.com>
2024-06-11 12:18:03 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { RouterProvider } from "react-router-dom";
 import { AuthProdiver } from "./authProvider/AuthProvider";
 import "./index.css";
 import Router from "./router/Router.jsx";
+import AllUsers from "./pages/dashboard/admin/users/AllUsers.jsx";
+import EditUser from "./pages/dashboard/admin/users/EditUser.jsx";
 
 const queryClient = new QueryClient();
 
